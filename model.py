c375ceb856dd78f241fa96a97c59da579136ebcf
Niyantha <maruthup@usc.edu>
2024-08-05 01:25:19 -0700

    Implemented Encoder

--- model.py
+++ model.py
@@ -148,3 +148,39 @@ class ResidualConnection(nn.Module):
 
     def forward(self, x, sublayer):
         return x + self.dropout(sublayer(self.norm(x)))
+
+
+class EncoderBlock(nn.Module):
+
+    def __init__(
+        self,
+        self_attention: MultiHeadAttention,
+        feed_forward: FeedForwardBlock,
+        dropout: float,
+    ) -> None:
+        super().__init__()
+        self.self_attention = self_attention
+        self.feed_forward = feed_forward
+        self.residual_connections = nn.ModuleList(
+            [ResidualConnection(dropout) for _ in range(2)]
+        )
+
+    def forward(self, c, src_mask):
+        x = self.residual_connections[0](
+            x, lambda x: self.self_attention(x, x, x, src_mask)
+        )
+        x = self.residual_connections[1](x, self.feed_forward)
+        return x
+
+
+class Encoder(nn.Module):
+
+    def __init__(self, layers: nn.ModuleList) -> None:
+        super().__init__()
+        self.layers = layers
+        self.norm = LayerNormalization()
+
+    def forward(self, x, mask):
+        for layer in self.layers:
+            x = layer(x, mask)
+        return self.norm(x)
